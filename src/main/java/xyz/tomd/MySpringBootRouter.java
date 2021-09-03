4294fd4930d60887d2bd2b623554b0c0bb138ebf
zaki <zaki.hendarto@gmail.com>
2021-09-03 14:44:55 +0700

    update

--- src/main/java/xyz/tomd/MySpringBootRouter.java
+++ src/main/java/xyz/tomd/MySpringBootRouter.java
@@ -1,15 +1,19 @@
 package xyz.tomd;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MySpringBootRouter extends RouteBuilder {
 
+    @Value("${greeting}")
+    private String greetingMessage; 
+	
     @Override
     public void configure() {
         from("servlet:/hello")
-                .setBody(constant("Hello world!"));
+                .setBody(constant(greetingMessage));
     }
 
 }
